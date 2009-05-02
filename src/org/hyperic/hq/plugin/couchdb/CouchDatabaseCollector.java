11c0bf3577f490100d7937e554fe419feef47a07
Doug MacEachern <dougm@hyperic.com>
2009-05-02 08:26:52 -0700

    setSource

--- src/org/hyperic/hq/plugin/couchdb/CouchDatabaseCollector.java
+++ src/org/hyperic/hq/plugin/couchdb/CouchDatabaseCollector.java
@@ -45,6 +45,7 @@ public class CouchDatabaseCollector extends Collector {
         this.hostname = props.getProperty(PROP_HOSTNAME);
         this.port = Integer.valueOf(props.getProperty(PROP_PORT));
         this.dbname = props.getProperty(PROP_DBNAME);
+        setSource(this.dbname);
         super.init();
     }
 
