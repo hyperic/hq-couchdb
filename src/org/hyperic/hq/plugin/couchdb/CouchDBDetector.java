39ed8acf563aacef74f8d0a9c7266f7e723f1c29
Doug MacEachern <dougm@hyperic.com>
2009-05-02 18:46:31 -0700

    add version 0.9 support

--- src/org/hyperic/hq/plugin/couchdb/CouchDBDetector.java
+++ src/org/hyperic/hq/plugin/couchdb/CouchDBDetector.java
@@ -30,6 +30,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -48,9 +49,19 @@ import org.jcouchdb.db.ServerImpl;
 
 public class CouchDBDetector extends DaemonDetector {
 
-    private static final String BIND_ADDRESS = "bindaddress";
+    private static final String BIND_ADDRESS = "httpd.bind_address";
+    private static final String HTTPD_PORT = "httpd.port";
     private static final String PROP_VERSION = "version";
-    private Map currentOptions;
+    private static final String OPT_COUCHINI = "-couchini";
+    private static final String SERVER_START =
+        "couch_server:start(";
+    //0.8 -> 0.9
+    private static final String[][] ALIASES = {
+        { "couch.logfile", "log.file" },
+        { "couch.bindaddress", BIND_ADDRESS },
+        { "couch.port", HTTPD_PORT }
+    };
+    private Map opts;
 
     private void setOpt(Map opts, String opt, String val) {
         if (opts.containsKey(opt) || //set by process                                                         
@@ -76,7 +87,7 @@ public class CouchDBDetector extends DaemonDetector {
             if (opt == null) {
                 continue;
             }
-            String val = (String)this.currentOptions.get(opt);
+            String val = (String)this.opts.get(opt);
             if (val != null) {
                 config.setValue(name, val);
             }
@@ -86,15 +97,119 @@ public class CouchDBDetector extends DaemonDetector {
         return server;
     }
 
-    protected Map getProcOpts(long pid) {
-        Map opts = super.getProcOpts(pid);
-        String defaultPort = getTypeProperty(Collector.PROP_PORT);
-        this.currentOptions = opts;
+    private boolean parseIni(String file) {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+            String line, section = null;
+            while ((line = in.readLine()) != null) {
+                line = line.trim();
+                if ((line.length() == 0) || (line.charAt(0) == ';')) {
+                    continue;
+                }
+                if (line.charAt(0) == '[') {
+                    section = xtrim(line, ']').toLowerCase();
+                }
+                String[] elts = line.split("=");
+                if (elts.length == 2) {
+                    String key = elts[0].toLowerCase().trim();
+                    if (!key.startsWith("_")) {
+                        opts.put(section + "." + key, elts[1].trim());
+                    }
+                }
+            }
+            getLog().debug("Parsed ini: " + file);
+            return true;
+        } catch (IOException e) {
+            getLog().error("Failed to read " + file + e.getMessage(), e);
+            return false;
+        } finally {
+            if (in != null) {
+                try { in.close(); } catch (IOException e) {}
+            }
+        }
+    }
 
+    private String xtrim(String str, char c) {
+        str = str.trim();
+        int ix = str.lastIndexOf(c);
+        if (ix == -1) {
+            return str;
+        }
+        return str.substring(1, ix).trim();
+    }
+
+    //0.9 style
+    //couch_server:start([ "couchdb/etc/couchdb/default.ini", \
+    //                     "couchdb/etc/couchdb/local.ini"]), \
+    //                   receive done -> done end.<=
+    private void parseOpts(long pid) {
+        String cwd = getProcCwd(pid);
+        String[] args = getProcArgs(pid);
+        String[] files = null;
+        for (int i=0; i<args.length; i++) {
+            String arg = args[i];
+            if (!arg.startsWith(SERVER_START)) {
+                continue;
+            }
+            arg = arg.substring(SERVER_START.length());
+            arg = xtrim(arg, ']');
+            files = arg.split("[,\\s]+");
+            break;
+        }
+        if (files == null) {
+            getLog().warn("Unable to find .ini files for pid=" + pid);
+            return;
+        }
+        if (cwd == null) {
+            File root = new File(args[0]).getParentFile(); //beam
+            while (root != null) {
+                if (new File(root, files[0]).exists()) {
+                    cwd = root.getPath();
+                    break;
+                }
+                root = root.getParentFile();
+            }
+        }
+        if (cwd == null) {
+            getLog().warn("Unable to find determine cwd for pid=" + pid);
+            return;
+        }
+
+        setOpt(opts, INSTALLPATH, cwd);
+        StringBuilder configs = new StringBuilder();
+        for (int i=0; i<files.length; i++) {
+            String arg = xtrim(files[i], '"');
+            String ini = cwd + File.separator + arg;
+            if (parseIni(ini)) {
+                if (configs.length() != 0) {
+                    configs.append(',');
+                }
+                configs.append(arg);
+            }
+        }
+        opts.put(OPT_COUCHINI, configs.toString()); //config_track
+    }
+
+    //0.8 style
+    private void parseOpts(String ini) {
+        parseIni(ini);
+        for (int i=0; i<ALIASES.length; i++) {
+            String value = (String)opts.remove(ALIASES[i][0]);
+            if (value != null) {
+                opts.put(ALIASES[i][1], value);
+            }
+        }
         String home = (String)opts.get("-home");
         if (home != null) {
             setOpt(opts, INSTALLPATH, home);
         }
+    }
+
+    protected Map getProcOpts(long pid) {
+        this.opts = super.getProcOpts(pid);
+        String defaultPort = getTypeProperty(Collector.PROP_PORT);
+
         setOpt(opts, INVENTORY_ID, "couchdb@%port%");
 
         String pidfile = (String)opts.get("-pidfile");
@@ -103,44 +218,22 @@ public class CouchDBDetector extends DaemonDetector {
                      "Pid.PidFile.eq=" + pidfile);
         }
 
-        //parse couchdb.ini
-        String ini = (String)opts.get("-couchini");
-
-        if (ini == null) {
-            getLog().debug("Unable to find couchdb.conf");
-            return opts;
+        String ini = (String)opts.get(OPT_COUCHINI);
+        if (ini != null) {
+            parseOpts(ini);
         }
-
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new FileReader(ini));
-            String line;
-            while ((line = in.readLine()) != null) {
-                line = line.trim();
-                if ((line.length() == 0) || (line.charAt(0) == ';')) {
-                    continue;
-                }
-                String[] elts = line.split("=");
-                if (elts.length == 2) {
-                    opts.put(elts[0].toLowerCase(), elts[1]);
-                }
-            }
-        } catch (IOException e) {
-            getLog().error("Failed to read " + ini + e.getMessage(), e);
-        } finally {
-            if (in != null) {
-                try { in.close(); } catch (IOException e) {}
-            }
+        else {
+            parseOpts(pid);
         }
 
         if (NetFlags.ANY_ADDR.equals(opts.get(BIND_ADDRESS))) {
             opts.put(BIND_ADDRESS, NetFlags.LOOPBACK_ADDRESS);
         }
-        if (opts.get(Collector.PROP_PORT) == null) {
-            opts.put(Collector.PROP_PORT, defaultPort);
+        if (opts.get(HTTPD_PORT) == null) {
+            opts.put(HTTPD_PORT, defaultPort);
         }
         //append http port for unique name
-        if (!opts.get(Collector.PROP_PORT).equals(defaultPort)) {
+        if (!opts.get(HTTPD_PORT).equals(defaultPort)) {
             setOpt(opts, AUTOINVENTORY_NAME,
                    "%platform.name% " + getTypeInfo().getName() + " @%port%");
         }
